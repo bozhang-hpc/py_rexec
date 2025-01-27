59c76dff9e83d84b20a10e7a368a15213d9f209b
Bo Zhang <bozhang@sci.utah.edu>
2025-01-27 15:29:10 -0700

    add the remote exec decorator for easy user interface

--- rexec_client.py
+++ rexec_client.py
@@ -2,41 +2,39 @@ import dill as pickle
 import sys
 import zmq
 
+def remote_exec(fn):
+    def remote_fn(*args):
+        pfn = pickle.dumps(fn)
+        pargs = pickle.dumps(args)
+
+        zmq_context = zmq.Context()
+        zmq_socket = zmq_context.socket(zmq.REQ)
+        zmq_socket.connect("tcp://localhost:5559")
+
+        zmq_mp_msg = [pfn, pargs]
+        zmq_socket.send_multipart(zmq_mp_msg)
+
+        zmq_ret_msg = zmq_socket.recv()
+        ret_msg = pickle.loads(zmq_ret_msg)
+
+        zmq_socket.disconnect("tcp://localhost:5559")
+        zmq_socket.close()
+        zmq_context.destroy()
+
+        return ret_msg
+    return remote_fn
+
+@remote_exec
 def hello_world():
     print("hello world!")
 
+@remote_exec
 def add(a, b):
     return a+b
 
 if __name__ == "__main__":
-    fn_ser = pickle.dumps(hello_world)
-    print(sys.getsizeof(fn_ser))
-    pickle.loads(fn_ser)()
+    ret = hello_world()
+    print(ret)
 
-    number = pickle.dumps(1)
-
-    mp_msg = [number, fn_ser]
-
-    zmq_context = zmq.Context()
-    zmq_socket = zmq_context.socket(zmq.REQ)
-    zmq_socket.connect("tcp://localhost:5559")
-
-    zmq_socket.send_multipart(mp_msg)
-    zmq_msg = zmq_socket.recv()
-    print(f"Received reply [ {zmq_msg} ]")
-
-    fn_add_ser = pickle.dumps(add)
-    add_args = [1, 2]
-    add_args_ser = pickle.dumps(add_args)
-
-    number = pickle.dumps(2)
-    mp_msg = [number, fn_add_ser, add_args_ser]
-    zmq_socket.send_multipart(mp_msg)
-    zmq_msg = zmq_socket.recv()
-    result = pickle.loads(zmq_msg)
-    print(f"Received remote add result [ {result} ]")
-
-
-    zmq_socket.disconnect("tcp://localhost:5559")
-    zmq_socket.close()
-    zmq_context.destroy()
+    ret = add(1,2)
+    print(ret)
--- rexec_server.py
+++ rexec_server.py
@@ -2,9 +2,21 @@ import time
 import zmq
 import dill as pickle
 
-class HelloRPC(object):
-    def hello(self, name):
-        return "Hello, %s" % name
+def fn_recv_exec(zmq_socket):
+    while(True):
+        zmq_msg = zmq_socket.recv_multipart()
+        fn = pickle.loads(zmq_msg[0])
+
+        args = pickle.loads(zmq_msg[1])
+
+        try:
+            ret = fn(*args)
+        except:
+            ret = "Exception Happens!"
+
+        pret = pickle.dumps(ret)
+
+        zmq_socket.send(pret)
 
 
 if __name__ == "__main__":
@@ -13,30 +25,29 @@ if __name__ == "__main__":
     zmq_socket = zmq_context.socket(zmq.REP)
     zmq_socket.connect("tcp://localhost:5560")
 
-    zmq_msg = zmq_socket.recv_multipart()
-    number = pickle.loads(zmq_msg[0])
-    print(f"Received request: {number}")
-    fn_ser = zmq_msg[1]
-    pickle.loads(fn_ser)()
+    try:
+        fn_recv_exec(zmq_socket)
+    except KeyboardInterrupt:
+        print("W: interrupt received, stopping rexec server...")
+    finally:
+        zmq_socket.disconnect("tcp://localhost:5560")
+        zmq_socket.close()
+        zmq_context.destroy()
 
-    time.sleep(1)
-
-    zmq_socket.send_string("World")
+    # zmq_socket.send_string("World")
     # server.stop()
 
-    zmq_msg = zmq_socket.recv_multipart()
-    number = pickle.loads(zmq_msg[0])
-    print(f"Received request: {number}")
-    fn_ser = zmq_msg[1]
-    fn_args_ser = zmq_msg[2]
-    fn_args = pickle.loads(fn_args_ser)
-    print(fn_args)
-    result = pickle.loads(fn_ser)(*fn_args)
-    print(result)
-    result_ser = pickle.dumps(result)
+    # zmq_msg = zmq_socket.recv_multipart()
+    # number = pickle.loads(zmq_msg[0])
+    # print(f"Received request: {number}")
+    # fn_ser = zmq_msg[1]
+    # fn_args_ser = zmq_msg[2]
+    # fn_args = pickle.loads(fn_args_ser)
+    # print(fn_args)
+    # result = pickle.loads(fn_ser)(*fn_args)
+    # print(result)
+    # result_ser = pickle.dumps(result)
 
-    zmq_socket.send(result_ser)
+    # zmq_socket.send(result_ser)
 
-    zmq_socket.disconnect("tcp://localhost:5560")
-    zmq_socket.close()
-    zmq_context.destroy()
+    

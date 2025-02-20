076fa01ff34b2b1b582297c167946653fac7a6f7
Bo Zhang <bozhang@sci.utah.edu>
2025-02-20 15:27:35 -0700

    add argparse for client example

--- rexec_client.py
+++ rexec_client.py
@@ -1,3 +1,4 @@
+import argparse
 from client_api.rexec import remote_exec
 
 @remote_exec
@@ -9,8 +10,22 @@ def add(a, b):
     return a+b
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "broker_ip", type=str,
+        help="The broker's ip address to connect. In [0-255].[0-255].[0-255].[0-255] format."
+    )
+
+    parser.add_argument(
+        "--broker_port", type=str, default="5559",
+        help="The broker's port to connect. [0-65535]"
+    )
+
     ret = hello_world()
     print(ret)
 
     ret = add(1,2)
+    print(ret)
+
+    ret = add(18,33)
     print(ret)
